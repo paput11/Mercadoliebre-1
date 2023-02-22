4b1b42a6af308616405bb7ebcffee1a3681fea6b
paput11 <paput11@gmail.com>
2023-02-22 20:08:36 -0300

    fix: arregla public

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 app.listen(3000,()=> (
     console.log("Servidor escuchando en el puerto" + port)
 ));
-app.use(express.static("public"));
+app.use(express.static("../public"));
     
 app.get ("/", (req,res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"));
